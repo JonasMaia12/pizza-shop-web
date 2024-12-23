57b63cf074ccd2ce195ecc7e94340cae40cab7b1
JonasMaia12 <jonaspiresmaia7@gmail.com>
2024-12-23 17:28:45 +0000

    add routes

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { createHashRouter } from "react-router-dom";
+import { Dashboard } from "./pages/app/DashBoard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
+  },
+
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [{ path: "/sign-in", element: <SignIn /> }],
+  },
+]);
