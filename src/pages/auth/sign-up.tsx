ab22ffed8c4494a6949848cdc5a4111e6d6a7fb1
JonasMaia12 <jonaspiresmaia7@gmail.com>
2024-12-29 14:19:36 +0000

    Add authenticate

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
 
+import { registerRestaurant } from "@/api/register-restaurant";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -26,14 +28,23 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<signUpForm>();
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  });
+
   async function handleSignUp(data: signUpForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      });
 
       toast.success("Restaurante cadastrado com sucesso!", {
         action: {
           label: "Login",
-          onClick: () => navigate("/sign-in"),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       });
     } catch {
@@ -79,13 +90,13 @@ export function SignUp() {
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="phone">Seu E-mail</Label>
-              <Input id="phone" type="tel" {...register("phone")} />
+              <Label htmlFor="email">Seu E-mail</Label>
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="email">Seu celular</Label>
-              <Input id="email" type="email" {...register("email")} />
+              <Label htmlFor="phone">Seu celular</Label>
+              <Input id="phone" type="tel" {...register("phone")} />
             </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
