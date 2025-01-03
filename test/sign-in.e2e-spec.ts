64c3a00ed5a735b98e6f7bc296852e1e2502aba6
JonasMaia12 <jonaspiresmaia7@gmail.com>
2025-01-03 16:28:48 +0000

    add Playwright and create e2e tests

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from "@playwright/test";
+
+test("sign in successfully", async ({ page }) => {
+  await page.goto("/#/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByLabel("Seu E-mail").fill("johndoe@example.com");
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText(
+    "Enviamos um link de autenticação para o seu email.",
+  );
+
+  expect(toast).toBeVisible();
+});
+
+test("sign in with wrong credentials", async ({ page }) => {
+  await page.goto("/#/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByLabel("Seu E-mail").fill("wrong@example.com");
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText(
+    "Não foi possível fazer o login. Tente novamente.",
+  );
+
+  expect(toast).toBeVisible();
+});
+
+test("navigate to new restaurant page", async ({ page }) => {
+  await page.goto("/#/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByRole("link", { name: "Novo estabelecimento" }).click();
+
+  expect(page.url()).toContain("/sign-up");
+});
